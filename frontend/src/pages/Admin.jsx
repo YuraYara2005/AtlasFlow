c08960882b34f18d85ccd3e2f1e521a835954b8b
Yara <yaramohammmed11@gmail.com>
2026-04-17 00:35:29 +0200

    implement admin page Admin.jsx

---
+++ frontend/src/pages/Admin.jsx
@@ -0,0 +1,10 @@
+// src/pages/Admin.jsx
+import AdminPanel from "@/features/admin/AdminPanel";
+
+export default function Admin() {
+  return (
+    <div className="w-full h-full">
+      <AdminPanel />
+    </div>
+  );
+}
